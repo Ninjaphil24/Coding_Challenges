f3ca90ed20a4f67b109368acfbf2c9fd2f0c0eb4
Ninjaphil24 <philipmodino@gmail.com>
2022-03-26 10:23:52 +0100

    Diagonal Difference works on Quoka

--- challenge.js
+++ challenge.js
@@ -1,17 +1,26 @@
-a = [1, 2, 3]
-b = [3, 2, 1]
+arr = [
+    3,
+    [11,2,4],
+    [4,5,6],
+    [10,8,-12]
+]
 
-function compareTriplets(a, b) {
+console.log(arr[0])
+
+function diagonalDifference(arr) {
     // Write your code here
-let Alice = 0;
-let Bob = 0;
-let result = []
-for(let i = 0; i<3;i++){
-    if(a[i] > b[i])Alice++
-    if(a[i] < b[i])Bob++
+    let diag1 = 0
+    let diag2 = 0
+    for(var i = 0, j=arr[0]-1;i<arr[0];i++,j--){
+        diag1 += arr[i+1][i]
+        diag2 += arr[i+1][j]
+        console.log(diag1)
+        console.log(diag2)
+        console.log(i)
+        console.log(j)
 }
-result.push(Alice,Bob)
-return result
+    let difference = diag1-diag2
+    console.log(difference)
+    return Math.abs(difference)
 }
-
-console.log(compareTriplets(a,b));
+console.log(diagonalDifference(arr));
